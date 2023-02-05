918b72caac5c0d080285a8162ca74d4b32e5a052
ArthurDev <arthurdevoficial@gmail.com>
2023-02-04 23:07:02 -0300

    feat: Added first clock form prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClockFormComponent } from './clock-form/clock-form.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+  		ClockFormComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		ReactiveFormsModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
---
+++ src/app/clock-form/clock-form.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+type ClockFormType = {
+	cycleCount: FormControl<number>,
+	workTime: FormControl<number>,
+	restTime: FormControl<number>,
+	finalRestTime: FormControl<number>,
+};
+
+@Component({
+	selector: 'app-clock-form',
+	templateUrl: './clock-form.component.html',
+	styleUrls: ['./clock-form.component.scss'],
+})
+export class ClockFormComponent implements OnInit {
+	clockForm: FormGroup<ClockFormType> = new FormGroup({
+		cycleCount: new FormControl(5, [
+			Validators.required,
+			Validators.min(0),
+			Validators.max(10),
+		]),
+		workTime: new FormControl(20, [
+			Validators.required,
+			Validators.min(20),
+			Validators.max(60),
+		]),
+		restTime: new FormControl(5, [
+			Validators.required,
+			Validators.min(5),
+			Validators.max(30),
+		]),
+		finalRestTime: new FormControl(20, [
+			Validators.required,
+			Validators.min(20),
+			Validators.max(60),
+		])
+	} as ClockFormType);
+
+	ngOnInit(): void {
+
+	}
+
+	startClock(): void {
+		alert('Button working!');
+		alert(this.clockForm.get('cycleCount')?.value);
+	}
+
+	get cycleCount() { return this.clockForm.get('cycleCount') };
+	get workTime() { return this.clockForm.get('workTime') };
+	get restTime() { return this.clockForm.get('restTime') };
+	get finalRestTime() { return this.clockForm.get('finalRestTime') };
+}
