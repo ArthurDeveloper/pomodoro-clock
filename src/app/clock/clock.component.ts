1d7faebeb401e0c139fae0ec72387baa5ffc204a
ArthurDev <arthurdevoficial@gmail.com>
2023-02-05 00:33:19 -0300

    feat: Implemented simple countdown in clock componente

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-clock',
 	templateUrl: './clock.component.html',
 	styleUrls: ['./clock.component.scss']
 })
-export class ClockComponent {
+export class ClockComponent implements OnInit {
+	minutes: number = 5;
+	seconds: number = 59;
+
+	ngOnInit(): void {
+		setInterval(() => {
+			if (this.seconds > 0) {
+				this.seconds--;
+			} else if (this.minutes > 0) {
+				this.minutes--;
+				this.seconds = 59;
+			}
+		}, 1000);
+	}
 
 }
