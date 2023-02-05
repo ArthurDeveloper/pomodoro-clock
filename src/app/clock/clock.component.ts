9b85e1988d6df9c40a374b87f2d250694243487e
ArthurDev <arthurdevoficial@gmail.com>
2023-02-05 14:54:08 -0300

    feat: Made simple toggle between rest and work times

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,14 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
 	selector: 'app-clock',
 	templateUrl: './clock.component.html',
 	styleUrls: ['./clock.component.scss']
 })
-export class ClockComponent implements OnInit {
-	@Input() minutes: number = 0;
+export class ClockComponent implements OnInit, OnChanges {
+	minutes: number = 0;
 	seconds: number = 0;
+	
 	@Input() active: boolean = false;
+
+	@Input() cycleCount: number = 0;
+	@Input() workTime: number = 0;
+	@Input() restTime: number = 0;
+
+	inRest: boolean = false;
 
 	ngOnInit(): void {
 		setInterval(() => {
@@ -20,6 +27,15 @@ export class ClockComponent implements OnInit {
 					this.seconds = 59;
 				}
 			}
+
+			if (this.minutes === 0) {
+				this.inRest = !this.inRest;
+				this.minutes = this.inRest ? this.restTime : this.workTime;
+			}
 		}, 1000);
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		this.minutes = this.inRest ? this.restTime : this.workTime;
 	}
 }
